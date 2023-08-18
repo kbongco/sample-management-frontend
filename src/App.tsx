15280ebe159464eb2e02f6901655b05084435e1a
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-08-18 11:42:26 -0400

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Screens/Login/Login';
+import NavBar from './Layout/NavBar/NavBar';
 
 function App() {
   return (
-      <Login/>
+    <NavBar/>
+      // <Login/>
   );
 }
 
